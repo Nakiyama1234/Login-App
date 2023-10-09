73f01bac68fd69d789ef17092bcdf1d0eb1b9de6
Nakiyama1234 <99705929+Nakiyama1234@users.noreply.github.com>
2023-10-09 09:25:29 +0700

    Sign In-Constrain Layout

---
+++ app/src/main/java/com/naufal/valblock/idn/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.naufal.valblock.idn.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naufal.valblock.idn.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/naufal/valblock/idn/dasboard/DashboardActiviti.kt
@@ -0,0 +1,12 @@
+package com.naufal.valblock.idn.dasboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naufal.valblock.idn.R
+
+class DashboardActiviti : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_activiti)
+    }
+}
